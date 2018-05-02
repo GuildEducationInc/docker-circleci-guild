cef83830f607d5e18f340ca9d19f671a8161ce56
Stafford Brunk <stafford@guildeducation.com>
2018-05-02 10:05:36 -0600

    Switch chromedriver install strategy to direct download
    The apt version doesn't appear to be as up to date

--- update.sh
+++ update.sh
@@ -24,6 +24,7 @@ VERSIONS=`cat versions.json`
 RUBY_VERSIONS=`echo ${VERSIONS} | jq -r '.ruby.versions[]'`
 NODE_VERSIONS=`echo ${VERSIONS} | jq -r '.node.versions[]'`
 CC_TEST_REPORTER_VERSION=`echo ${VERSIONS} | jq -r '.cc_test_reporter_version'`
+CHROMEDRIVER_VERSION=`echo ${VERSIONS} | jq -r '.chromedriver_version'`
 
 # Drop a Dockerfile for each Ruby version
 mkdir -p ruby && rm -rf ruby/*
@@ -47,5 +48,6 @@ for ruby_version in ${RUBY_VERSIONS}; do
     yarn_version=`echo ${VERSIONS} | jq -r ".node.yarn[\"${node_version}\"]"`
     sed -E -i '' 's/\$NODE_VERSION/'"${node_version}"'/' rails/${ruby_version}-${node_version}/Dockerfile
     sed -E -i '' 's/\$YARN_VERSION/'"${yarn_version}"'/' rails/${ruby_version}-${node_version}/Dockerfile
+    sed -E -i '' 's/\$CHROMEDRIVER_VERSION/'"${CHROMEDRIVER_VERSION}"'/' rails/${ruby_version}-${node_version}/Dockerfile
   done
 done
